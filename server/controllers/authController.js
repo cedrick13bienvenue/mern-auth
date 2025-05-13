91db8ace684e8d5527091e87e19f067c0bb7297f
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-05-13 21:44:30 +0200

    setting up cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,6 +21,12 @@ export const register = async (requestAnimationFrame, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expieresIn: "7d",
     });
+    res.cookie("token", token, {
+      httpOnly: true,
+      sucure: process.env.NODE_ENV === "production",
+      sameSite: (process.env.NODE_ENV = "production" ? "none" : "strict"),
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
