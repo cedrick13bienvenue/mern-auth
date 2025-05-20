0681b47837933efa5039674f38b7a46f17fe054d
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-05-20 21:27:10 +0200

    removed imported response

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
-import { response } from "express";
 import transporter from "../config/nodemailer.js";
 
 export const register = async (req, res) => {
