1b738b4eec83b522e32bc4d43d59d1d07cfb7229
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-05-28 11:52:15 +0200

    fix: change getUserData route from GET to POST
    - Change /data route from GET to POST to support request body
    - Add /all helper route for development testing
    - Import getAllUsers controller function

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import userAuth from "../middleware/userAuth";
-import { getUserData } from "../controllers/userController";
+import userAuth from "../middleware/userAuth.js";
+import { getUserData, getAllUsers } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/data", userAuth, getUserData);
+// Changed from GET to POST for getUserData
+userRouter.post("/data", userAuth, getUserData);
+
+// Helper route to see all users (for testing - remove in production)
+userRouter.get("/all", getAllUsers);
 
 export default userRouter;
-// defining a user router for handling user-related routes
