08b7bd37cef499f241a55919cd69bb83814dd8f4
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-05-30 22:57:23 +0200

    Configuring PostCSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg">
+      <h1 className="text-2xl font-bold">Hello Tailwind CSS!</h1>
+    </div>
+  );
 };
 
 export default App;
