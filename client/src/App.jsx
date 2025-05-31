64003dd6f08ee1188721649b2154ebc17e573872
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-05-31 08:42:10 +0200

    feat: add routing for home, login, email verification, and password reset pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
     <div>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/email-verify" element={<EmailVerify />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+      </Routes>
     </div>
   );
 };
