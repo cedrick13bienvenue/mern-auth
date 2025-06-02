c7b67153d2a0cf1f6c8a45411ac6b0639ae487fb
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-02 22:00:44 +0200

    Adding classes on some couple of divs on login file

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
         alt=""
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
-      <div>
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
         <h2>{state === "Sign Up" ? "Create Account" : "Login"}</h2>
         <p>
           {state === "Sign Up"
@@ -21,7 +21,12 @@ const Login = () => {
         <form>
           <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.person_icon} alt="" />
-            <input type="text" placeholder="Full Name" required />
+            <input
+              className="bg-transparent outline-none"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
           </div>
         </form>
       </div>
