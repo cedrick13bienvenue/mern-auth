e3ce42d38e4c5407b551c82d9632296eed0a36ed
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-02 22:07:12 +0200

    Adding email input field

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,6 +30,15 @@ const Login = () => {
               required
             />
           </div>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="" />
+            <input
+              className="bg-transparent outline-none"
+              type="email"
+              placeholder="Email ID"
+              required
+            />
+          </div>
         </form>
       </div>
     </div>
