73918eef7d0569b325b61d28b6cda01975fca0dd
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-02 22:49:19 +0200

    Setting up state variables to handle values from input field

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,6 +3,9 @@ import { assets } from "../assets/assets";
 
 const Login = () => {
   const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="flex items-center justify-center min-h-screen sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
@@ -25,6 +28,8 @@ const Login = () => {
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="" />
               <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className="bg-transparent outline-none"
                 type="text"
                 placeholder="Full Name"
@@ -35,6 +40,8 @@ const Login = () => {
           <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="" />
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="bg-transparent outline-none"
               type="email"
               placeholder="Email ID"
@@ -44,6 +51,8 @@ const Login = () => {
           <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.lock_icon} alt="" />
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="bg-transparent outline-none"
               type="password"
               placeholder="Password"
