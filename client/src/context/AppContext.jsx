4f25c2c2a30d1bdd9e4862373019550496072001
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-03 21:57:53 +0200

    Adding several state variables

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,17 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContent = createContext();
 
 export const AppContextProvider = (props) => {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [userData, setUserData] = useState(false);
   const value = {
     backendUrl,
+    isLoggedin,
+    setIsLoggedin,
+    userData,
+    setUserData,
   };
 
   return (
