802dc20ec99605960733c987d0ff5e81f9a74eb0
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-05 08:19:02 +0200

    Sign-Up functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,6 +11,26 @@ const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      axios.defaults.withCredentials = true;
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate("/");
+        } else {
+          alert(data.message);
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
@@ -29,7 +49,7 @@ const Login = () => {
             ? "Create your account"
             : "Login into your account"}
         </p>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {state === "Sign Up" && (
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="" />
