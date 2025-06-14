9ce36cb75cd895643a6a1097e5c6b8428721b4fd
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-14 20:17:04 +0200

    feat: integrate Swagger for API documentation with endpoint at /documentation

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/mondodb.js"; // Match your existing file name
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,6 +14,41 @@ const port = process.env.PORT || 4000;
 connectDB();
 
 const allowedOrigins = ["http://localhost:5173"];
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "mern-auth",
+      version: "1.0.0",
+      description: "app description",
+    },
+    schemes: ["http"],
+    host: "localhost:4000",
+    basePath: "/api",
+    securityDefinitions: {
+      bearerAuth: {
+        type: "apiKey",
+        name: "Authorization",
+        scheme: "bearer",
+        in: "header",
+      },
+    },
+  },
+  apis: ["./routes/*.js", "./controllers/*.js", "./model/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.get("/swagger.json", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerDocs);
+});
+
+app.use(
+  "/documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, false, {
+    docExpansion: "none",
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
