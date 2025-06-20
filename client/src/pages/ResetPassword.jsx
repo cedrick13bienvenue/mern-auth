e210480bc9e5c670809abb86b281e38f6011680a
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-20 16:36:32 +0200

    Adding form on the page

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -13,7 +13,15 @@ const ResetPassword = () => {
         alt=""
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
       />
-      <h1>Reseting Password</h1>
+      {/* enter email iD: */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password
+        </h1>
+        <p className="text-center mb-6 text-indigo-300">
+          Enter your registered email address
+        </p>
+      </form>
     </div>
   );
 };
