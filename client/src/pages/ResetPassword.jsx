95a8e5c69b3400b4ceb0260cd2e4883056936d0e
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-20 16:29:16 +0200

    setting up reset password page

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const ResetPassword = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt=""
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
+      />
       <h1>Reseting Password</h1>
     </div>
   );
