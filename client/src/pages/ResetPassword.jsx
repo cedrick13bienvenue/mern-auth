d04b9b40b978b3a5cf374bc148c1764ab67ec2a7
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-20 16:31:49 +0200

    Adding useNavigate() on the page

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
 
 const ResetPassword = () => {
   return (
