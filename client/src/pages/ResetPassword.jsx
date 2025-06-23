048d7f551963f474c86d500ae0b66201eff28f0d
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-23 23:26:08 +0200

    fix: typo while writing onSubmitEmail function

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -66,7 +66,7 @@ const ResetPassword = () => {
 
       {!isEmailSent && (
         <form
-          onClick={onSubmitEmail}
+          onSubmit={onSubmitEmail}
           className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm"
         >
           <h1 className="text-white text-2xl font-semibold text-center mb-4">
