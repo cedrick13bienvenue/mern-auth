302e53efb843a10db66544e49837139e4fedbfd6
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-06-29 14:43:08 +0200

    Importing Email Verify template

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
+import { EMAIL_VERIFY_TEMPLATE } from "../config/emailTemplates.js";
 
 export const register = async (req, res) => {
   try {
